775fe4467ea9d58c129720436bf6f3525a9fb866
David Sveningsson <ext@sidvind.com>
2010-07-23 00:47:36 +0200

    setuptools

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+setup(
+    name = "pylistener",
+    version = "0.1",
+    package_dir = {'pylistener': 'src'},
+    packages = ['pylistener'],
+    
+    author = "David Sveningsson",
+    author_email = "ext@sidvind.com",
+    description = "",
+    license = "BSD",
+    keywords = "",
+    url = "http://github.com/ext/pylistener",
+)
