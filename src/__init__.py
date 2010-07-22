42b7e10bb9e79059b0db41895f8b8b1cd94099fe
David Sveningsson <ext@sidvind.com>
2010-07-23 00:50:03 +0200

    only using test-classes if running the file

--- src/__init__.py
+++ src/__init__.py
@@ -63,50 +63,52 @@ class Listen:
             # finally, call the function
             f(*args, **kwargs)
 
-@interface
-class ListenA (Listen):
-    @event
-    def trigger(self, *args, **kwargs):
-        print 'ListenA::trigger', self.a
-
-    @event
-    def foo(self):
-        print 'ListenA::foo', self
-
-@interface
-class ListenB (Listen):
-    @event
-    def fred(self, a, b, c):
-        print 'ListenB::fred', a, b, c
-
-class ConcreteA (ListenA):
-    def __init__(self, value):
-        ListenA.__init__(self)
-        self.a = value
-
-    def trigger(self, *args, **kwargs):
-        print 'ConcreteA::trigger', self.a
-
-class ConcreteB (ListenB):
-    def fred(self, a,b,c):
-        print 'ConcreteB::fred', a,b,c
-
-class ConcreteB2 (ConcreteB):
-    def fred(self, a,b,c):
-        print 'ConcreteB2::fred', a,b,c
-
-class ConcreteAB(ConcreteA, ConcreteB):
-    def __init__(self, value):
-        ConcreteA.__init__(self, value)
-        ConcreteB.__init__(self)
-
-    def trigger(self, *args, **kwargs):
-        print 'ConcreteAB::trigger', self.a
-
-    def fred(self, a,b,c):
-        print 'ConcreteAB::fred', a,b,c
-
 if __name__ == '__main__':
+
+    @interface
+    class ListenA (Listen):
+        @event
+        def trigger(self, *args, **kwargs):
+            print 'ListenA::trigger', self.a
+            
+        @event
+        def foo(self):
+            print 'ListenA::foo', self
+            
+    @interface
+    class ListenB (Listen):
+        @event
+        def fred(self, a, b, c):
+            print 'ListenB::fred', a, b, c
+
+    class ConcreteA (ListenA):
+        def __init__(self, value):
+            ListenA.__init__(self)
+            self.a = value
+            
+        def trigger(self, *args, **kwargs):
+            print 'ConcreteA::trigger', self.a
+
+    class ConcreteB (ListenB):
+        def fred(self, a,b,c):
+            print 'ConcreteB::fred', a,b,c
+
+    class ConcreteB2 (ConcreteB):
+        def fred(self, a,b,c):
+            print 'ConcreteB2::fred', a,b,c
+
+    class ConcreteAB(ConcreteA, ConcreteB):
+        def __init__(self, value):
+            ConcreteA.__init__(self, value)
+            ConcreteB.__init__(self)
+
+        def trigger(self, *args, **kwargs):
+            print 'ConcreteAB::trigger', self.a
+
+        def fred(self, a,b,c):
+            print 'ConcreteAB::fred', a,b,c
+
+
     a = ConcreteA(1)
     d = ConcreteAB(2)
 
